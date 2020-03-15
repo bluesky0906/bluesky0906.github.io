f394776054b769fc5f04900254ed2b1bf8feb3cd
Sora Tagami <sora@SoranoMacBook-Air.local>
2020-03-15 19:06:33 +0900

    add fade

--- scroll.js
+++ scroll.js
@@ -1,23 +1,30 @@
-$(function() {
+$(function () {
   // スクロールのオフセット値
   var offsetY = -10;
   // スクロールにかかる時間
   var time = 2000;
 
   // ページ内リンクのみを取得
-  $('a[href^="#"]').click(function() {
+  $('a[href^="#"]').click(function () {
     // 移動先となる要素を取得
     var target = $(this.hash);
-    if (!target.length) return ;
+    if (!target.length) return;
     // mainのpadding-topを取得
     var main_padding_top = parseInt($('main').css('padding-top'), 10);
     // 移動先となる値
     var targetY = target.offset().top - main_padding_top;
     // スクロールアニメーション
-    $('html,body').animate({scrollTop: targetY}, time, 'easeInOutQuart');
+    $('html,body').animate({ scrollTop: targetY }, time, 'easeInOutQuart');
     // ハッシュ書き換えとく
     window.history.pushState(null, null, this.hash);
     // デフォルトの処理はキャンセル
     return false;
   });
 });
+
+$('head').append(
+  '<style>main{display:none;}'
+  );
+  $(window).on("load", function() {
+  $('main').delay(200).fadeIn(700);
+  });
